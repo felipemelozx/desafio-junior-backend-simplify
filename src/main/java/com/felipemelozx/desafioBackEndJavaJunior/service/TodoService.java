9362c13a95505caf2438d1d55fc52308cc5bf706
Felipe <felipemelozx@gmail.com>
2024-05-24 16:46:46 -0300

    feat: create todoService

---
+++ src/main/java/com/felipemelozx/desafioBackEndJavaJunior/service/TodoService.java
@@ -0,0 +1,36 @@
+package com.felipemelozx.desafioBackEndJavaJunior.service;
+
+import com.felipemelozx.desafioBackEndJavaJunior.entity.Todo;
+import com.felipemelozx.desafioBackEndJavaJunior.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> create( Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List<Todo> list() {
+    Sort sort = Sort.by("priority").descending().and(
+            Sort.by("name").ascending());
+        return todoRepository.findAll();
+    }
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return list();
+    }
+    public List<Todo> delete(Long id ){
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
