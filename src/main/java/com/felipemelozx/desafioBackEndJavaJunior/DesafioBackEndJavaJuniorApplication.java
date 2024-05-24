713aebdc512e50442affefe5830703e2b189d8e0
Felipe <felipemelozx@gmail.com>
2024-05-24 17:01:05 -0300

    add

---
+++ src/main/java/com/felipemelozx/desafioBackEndJavaJunior/DesafioBackEndJavaJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.felipemelozx.desafioBackEndJavaJunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndJavaJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndJavaJuniorApplication.class, args);
+	}
+
+}
